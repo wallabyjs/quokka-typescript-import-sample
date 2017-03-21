6d387af93c13877cc401323d4b76323f6ec792a8
Artem Govorov <artem.govorov@gmail.com>
2017-03-21 16:50:30 +1000

    Initial commit

---
+++ quokka.ts
@@ -0,0 +1,7 @@
+// Note that ts-node doesn't have to be installed in your project's node modules,
+// it may be installed in the quokka global folder. Typescript module is also not required.
+// See https://quokkajs.com/docs/configuration.html
+
+import value from './myModule';
+
+console.log(value);
